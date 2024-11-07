5ed122a4f4c58295838f9df39d9319bcbd45a9bc
guyevaristebade <guy.badepro@gmail.com>
2024-11-07 10:54:47 +0100

    modification des variable d'environnement

--- client/src/utils/instance.ts
+++ client/src/utils/instance.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosInstance} from 'axios';
 
 export const instance : AxiosInstance = axios.create({
-    baseURL: 'http://localhost:2024/api',
+    baseURL : process.env.REACT_APP_BASEURL_HTPPS || process.env.REACT_APP_BASEURL_LOCAL,
     withCredentials : true
 })
--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(compression())
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true
 }));
 
