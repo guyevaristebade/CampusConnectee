cdd4863d9badf5eca64a2c96d8d24058f298ed63
guyevaristebade <guy.badepro@gmail.com>
2024-12-09 14:24:30 +0100

    nouveau test de déploiement

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,5 @@
 export * from './user'
 export * from './attendance'
-export * from './file'
 export * from './student'
 export * from './admin'
 export * from './statistics'
--- server/src/utils/createUserWithFile.ts
+++ server/src/utils/createUserWithFile.ts
@@ -36,18 +36,55 @@ export const createStudentWithCsvFile = async (filePath: string) => {
 
 
 
+// export const createStudentWithXlsxFile = async (filePath: string) => {
+
+//     const fileBuffer : Buffer = fs.readFileSync(filePath);
+    
+//     const workbook : XLSX.WorkBook = XLSX.read(fileBuffer, { type: 'buffer' });
+
+//     const worksheet : XLSX.WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
+    
+//     const studentsData :  any[] = XLSX.utils.sheet_to_json(worksheet);
+
+    
+//     for (const row  of studentsData) {
+//         const last_name = row['NOM'];
+//         const first_name = row['PRENOM'];
+
+//         try {
+//             const existingStudent = await Student.findOne({ last_name, first_name });
+
+//             if (!existingStudent) {
+//                 const student = new Student({
+//                     last_name,
+//                     first_name,
+//                 });
+
+//                 await student.save();
+//                 console.log(`Étudiant ajouté : ${first_name} ${last_name}`);
+//             } else {
+//                 console.log(`Étudiant déjà existant : ${first_name} ${last_name}`);
+//             }
+//         } catch (error : any) {
+//             console.error(`Erreur lors de la vérification ou de la création de l'étudiant : ${error}`);
+//         }
+//     }
+
+//     console.log('Le fichier XLSX a été traité avec succès');
+// };
+
 export const createStudentWithXlsxFile = async (filePath: string) => {
+    const fileBuffer: Buffer = fs.readFileSync(filePath);
 
-    const fileBuffer : Buffer = fs.readFileSync(filePath);
-    
-    const workbook : XLSX.WorkBook = XLSX.read(fileBuffer, { type: 'buffer' });
+    // Convert Buffer to Uint8Array
+    const arrayBufferView = new Uint8Array(fileBuffer);
 
-    const worksheet : XLSX.WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
-    
-    const studentsData :  any[] = XLSX.utils.sheet_to_json(worksheet);
+    const workbook: XLSX.WorkBook = XLSX.read(arrayBufferView, { type: 'array' });
 
-    
-    for (const row  of studentsData) {
+    const worksheet: XLSX.WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
+    const studentsData: any[] = XLSX.utils.sheet_to_json(worksheet);
+
+    for (const row of studentsData) {
         const last_name = row['NOM'];
         const first_name = row['PRENOM'];
 
@@ -65,7 +102,7 @@ export const createStudentWithXlsxFile = async (filePath: string) => {
             } else {
                 console.log(`Étudiant déjà existant : ${first_name} ${last_name}`);
             }
-        } catch (error : any) {
+        } catch (error: any) {
             console.error(`Erreur lors de la vérification ou de la création de l'étudiant : ${error}`);
         }
     }
